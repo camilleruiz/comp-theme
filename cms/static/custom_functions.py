93c9beb260fe35d9a345e29f1f9ad781c231c609
Camille <camillemarieruiz@gmail.com>
2019-07-23 14:47:19 +0800

    change logo, change course to challenge

---
+++ cms/static/custom_functions.py
@@ -0,0 +1,6 @@
+<script type="loncapa/python">
+def is_equivalent_without_spaces( answer, candidate ):
+	ans = answer.replace(" ","")
+	cand = candidate.replace(" ","")
+	return ans == cand
+</script>
